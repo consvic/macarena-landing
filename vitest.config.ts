bb6c82b58c58e92ded8b5e51d2b7cfeeefdc024d
consvic <consvic@users.noreply.github.com>
2026-03-08 12:49:46 -0600

    add testing

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  esbuild: {
+    jsx: "automatic",
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/test/setup.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
